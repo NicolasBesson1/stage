698b79220283a7b42ef3db8284f057f9a10d0df8
NicolasBesson1 <58178659+NicolasBesson1@users.noreply.github.com>
2020-06-16 13:00:23 +0200

    Update Stage.py

--- Stage.py
+++ Stage.py
@@ -1,25 +1,3 @@
-
-'''
-#Test ISL
-space = isl.Space.create_from_names(isl.DEFAULT_CONTEXT, set=["x", "y"])
-
-bset = (isl.BasicSet.universe(space)
-        .add_constraint(isl.Constraint.ineq_from_names(space, {1: -1, "x": 1}))
-        .add_constraint(isl.Constraint.ineq_from_names(space, {1: 5, "x": -1}))
-        .add_constraint(isl.Constraint.ineq_from_names(space, {1: -1, "y": 1}))
-        .add_constraint(isl.Constraint.ineq_from_names(space, {1: 5, "y": -1})))
-print("set 1 %s:" % bset)
-
-bset2 = isl.BasicSet("{[x, y] : x >= 0 and x < 5 and y >= 0 and y < x+4 }")
-print("set 2: %s" % bset2)
-
-bsets_in_union = []
-bset.union(bset2).convex_hull().foreach_basic_set(bsets_in_union.append)
-print(bsets_in_union)
-union, = bsets_in_union
-print("union: %s" % union)
-'''
-
 from z3 import *
 from random import randint
 from islpy import *
@@ -71,3 +49,25 @@ def polyedre(n=3,m=3):
         return S
 
 print(polyedre())
+
+
+'''
+#Test ISL
+space = isl.Space.create_from_names(isl.DEFAULT_CONTEXT, set=["x", "y"])
+
+bset = (isl.BasicSet.universe(space)
+        .add_constraint(isl.Constraint.ineq_from_names(space, {1: -1, "x": 1}))
+        .add_constraint(isl.Constraint.ineq_from_names(space, {1: 5, "x": -1}))
+        .add_constraint(isl.Constraint.ineq_from_names(space, {1: -1, "y": 1}))
+        .add_constraint(isl.Constraint.ineq_from_names(space, {1: 5, "y": -1})))
+print("set 1 %s:" % bset)
+
+bset2 = isl.BasicSet("{[x, y] : x >= 0 and x < 5 and y >= 0 and y < x+4 }")
+print("set 2: %s" % bset2)
+
+bsets_in_union = []
+bset.union(bset2).convex_hull().foreach_basic_set(bsets_in_union.append)
+print(bsets_in_union)
+union, = bsets_in_union
+print("union: %s" % union)
+'''
