443a29118e111d52c2de5e8ef818f6f292e79bd3
NicolasBesson1 <58178659+NicolasBesson1@users.noreply.github.com>
2020-07-17 11:50:19 +0200

    Update CooperZ3.py

--- CooperZ3.py
+++ CooperZ3.py
@@ -1,4 +1,5 @@
 from z3 import *
+
 '''METHODE DE COOPER'''
 def step1(F):
         #Mettre F en nnf
@@ -45,7 +46,9 @@ def step2(F):
                                 
                                 if(str(children[0].decl())=="=="):
                                         # Si F est de la forme Not(A%B==0) on laisse la formule telle qu'elle est
-                                        if(str(grand_children[0].decl())=="%" or str(grand_children[1].decl())=="%"):
+                                        if(len(grand_children[0].children())==2 and str(grand_children[0].decl())=="%"):
+                                            return Not(children[0])
+                                        if(len(grand_children[1].children())==2 and str(grand_children[1].decl())=="%"):
                                             return Not(children[0])
                                         A=grand_children[0]
                                         B=grand_children[1]
@@ -61,7 +64,7 @@ def step2(F):
                                         A=grand_children[0]
                                         B=grand_children[1]
                                         return A<B+1
-                return F.decl()(step2(children[0])
+                return F.decl()(step2(children[0]))
         A=children[0]
         B=children[1]         
         if(str(F.decl())=="<="):
@@ -84,14 +87,84 @@ def step2(F):
                 result=F.decl()(result,step2(children[i]))
 
         return result
-         
+
+
+def sum_coefficients(F,x):
+    print("tetita = " + str(Var(0,s=Int)))
+    children=F.children()
+    if(F==x):
+        return 1
+ 
+    if(len(children)==0 or len(children)==1 or len(children)>2):
+        return 0
+    
+    if(str(F.decl())=="*"):
+        if(children[0]==x):
+            return children[1]
+        elif(children[1]==x):
+            return children[0]
+        else:
+            return 0  
+    
+    if(str(F.decl())=="+"):
+        return sum_coefficients(children[0],x)+sum_coefficients(children[1],x)
+    return 0
+
+
+def contains_x(F,x):
+    children=F.children()
+    if(len(children)==0):
+        return x==F
+
+    result=False
+    for i in children:
+        result=result or contains_x(i,x)
+    return result
+    
+    
+
+def sum_constants(F,x):
+    print("conchita = " + str(x))
+    children=F.children()
+
+    if(len(children)==0):
+        if not(x==F):
+            return F
+        return 0
+
+    if(len(children)==1 or len(children)>2):
+        return 0
+    
+    if(str(F.decl())=="+"):
+        return sum_constants(children[0],x)+sum_constants(children[1],x)
+    if(str(F.decl())=="*"):
+        if(contains_x(F,x)):
+            return 0
+        return F
+    return 0
+            
+            
+
+
+def h(F,x):
+    print("culo = " + str(x))
+    return sum_coefficients(F.children()[0],x)-sum_coefficients(F.children()[1],x)
+
+def t(F,x):
+    print("pito = " + str(x))
+    return sum_constants(F.children()[1],x)-sum_constants(F.children()[0],x)
+
+
+    
+
 def step3(F,x):
+        print("ano = " + str(x))
         children=F.children()
         #Si F est une variable ou une constante, pas de changement
         if(len(children)==0):
                 return F
         if(len(children)==1):
-                #Si F = Exists(x',F'(x')), on applique la methode de cooper sur F.
+                #Si F = Exists(x',F'(x',x)), on applique la methode de cooper sur F avec x comme constante.
                 if(is_exists(F.sexpr())):
                         return methode_cooper(F)
                 #Si F = Not(F'(x)), appel recurssif pour isoler les x dans F'(x)
@@ -102,34 +175,52 @@ def step3(F,x):
 
         #Si F est A<B on isole les x pour avoir hx<t      
         if(str(F.decl()) == "<"):
-            return h(children[0],children[1])*x < t(children[0], children[1])
+            return h(F,x)*x < t(F,x)
         #idem pour A>B
         if(str(F.decl()) == ">"):
-            return h(children[0],children[1])*x > t(children[0], children[1])
+            return h(F,x)*x > t(F,x)
         #Vu que les formules A==B ont ete elimines a l'etape 2, on sait qu'il s'agit d'une contrainte de divisibilite (a | b  <=> b%a==0)
-        #On isole les x pour avoir une formule de la forme k | hx+t 
+        #On simplifie a | b par Exists(l,l*a==b)
         if(str(F.decl()) == "=="):
+            l=Int("l")
             if(str(children[0].decl())=="%"):
                 grand_children=children[0].children()
-                
-        #A finir
-        
+                a=grand_children[0]
+                b=grand_children[1]
+            else:
+                grand_children=children[1].children()
+                a=grand_children[0]
+                b=grand_children[1]
+            return methode_cooper(Exists(l,b*l==a))
+            
+        #Si F est de la forme P(F1(x),F2(x), ... Fn(x)) on renvoit P(step3(F1,x),...,step3(Fn,x))        
+        result=step3(children[0],x)
+        for i in range(1,len(children)):
+            result=F.decl()(result,step3(children[i],x))
+        return result
 
 def methode_cooper(P):
         if(is_exists(P.sexpr())):
             F=P.children()[0]
+            x=Int(P.var_name(0))
+            
             #Step 1
             result=step1(F)
             #Step 2
             result=step2(result)
-            return result
+            print(result)
+            #Step 3
+            result=step3(result,x)
+            print(result)
+            return True
 
 
 
-
-
+x=Int("x")
 F=And(3*x+7<=18,Not(x==1))
-print(F.decl()(F.children()[0],F.children()[1]))
 
 
-print(methode_cooper(F,x))
+print(methode_cooper(Exists(x,F)))
+
+
+
